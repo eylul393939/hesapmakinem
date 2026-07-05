865b1ac4a077ff97309882a09bf4f9ea2f25db85
Catastrofun <catastrofungames@gmail.com>
2026-07-05 13:53:01 +0300

    zaman ekledim

---
+++ ilk_kod.py
@@ -0,0 +1,14 @@
+import random
+gizli_sayi = random.randint(1, 100)
+print("1 ile 100 arasında bir sayı tuttum bil bakalım")
+tahmin = None
+while tahmin != gizli_sayi:
+    tahmin = 4 
+import time
+başlangic_zamani = time.time()
+
+else:
+print("tebrikler! dogru bildiniz")
+bitis_zamani = time.time()
+gecen_sure = bitis_zamani - baslangic_zamani
+print(f"süre : {gecen_sure:.2F} saniye")
